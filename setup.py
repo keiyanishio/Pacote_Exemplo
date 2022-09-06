3a63faae18f4cdd9e4065e597722e62ad0297434
keiyanishio <keiyan@al.insper.edu.br>
2022-09-06 11:16:36 -0300

    pacotes

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
+#!/usr/bin/env python3
+
 from setuptools import setup
 
 setup(name='dev_aberto_keiya',
       version='0.1',
-      packages=['dev_aberto']
+      packages=['dev_aberto'],
+      install_requires=[
+        'pacote>=1.0',
+        'pacote2'
+      ],
+      scripts=['scripts/hello.py']
       )
